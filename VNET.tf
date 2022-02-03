5e87f7e494da1e0b13b9a21aa2ee0e7a393becaa
abdullaherol07 <abdullaherol07@gmail.com>
2022-02-03 20:55:53 +0000

    added

--- VNET.tf
+++ VNET.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "demovnet" {
   name                = "demovnet"
-  location            = azurerm_resource_group.Westeurope.location
+  location            = azurerm_resource_group.West Europe.location
   resource_group_name = azurerm_resource_group.demo.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
