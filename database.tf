e996039bb068e6b8a1fe1ff1d6ee3355d233d609
abdullah07 <abdullaherol07@gmail.com>
2022-02-11 17:16:34 -0500

    ss

--- database.tf
+++ database.tf
@@ -18,6 +18,7 @@ resource "azurerm_mysql_server" "wordpress" {
   public_network_access_enabled     = true
   ssl_enforcement_enabled           = false
   #ssl_minimal_tls_version_enforced = "TLS1_2"
+}  
 
 resource "azurerm_mysql_virtual_network_rule" "example" {
   name                = "mysql-vnet-rule"
