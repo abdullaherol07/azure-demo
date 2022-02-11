156cd5d187d83c5c4aff1bccfff1cc02f81a9707
abdullah07 <abdullaherol07@gmail.com>
2022-02-11 17:07:37 -0500

    changes

--- VNET.tf
+++ VNET.tf
@@ -1,50 +1,23 @@
-resource "azurerm_virtual_network" "demovnet" {
-  name                = "demovnet"
-  location            = "azurerm_resource_group.eastus.location"
-  resource_group_name = "azurerm_resource_group.demo.name"
+resource "azurerm_virtual_network" "wordpress" {
+  name                = "wordpress-vnet"
   address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.wordpress.name
+  tags                = var.tags
 }
 
-resource "azurerm_subnet" "public1" {
-  name                 = "public1-subnet"
-  resource_group_name  = "azurerm_resource_group.demo.name"
-  virtual_network_name = "azurerm_virtual_network.demovnet.name"
-  address_prefixes     = ["10.0.1.0/24"]
-}
-  
-
-resource "azurerm_subnet" "public2" {
-  name                 = "public2-subnet"
-  resource_group_name  = "azurerm_resource_group.demo.name"
-  virtual_network_name = "azurerm_virtual_network.demovnet.name"
+resource "azurerm_subnet" "wordpress" {
+  name                 = "wordpress-subnet"
+  resource_group_name  = azurerm_resource_group.wordpress.name
+  virtual_network_name = azurerm_virtual_network.wordpress.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-  resource "azurerm_subnet" "public3" {
-  name                 = "public3-subnet"
-  resource_group_name  = "azurerm_resource_group.demo"
-  virtual_network_name = "azurerm_virtual_network.demovnet.name"
-  address_prefixes     = ["10.0.3.0/24"]
-  }
-
-resource "azurerm_subnet" "private1" {
-  name                 = "private1-subnet"
-  resource_group_name  = "azurerm_resource_group.demo.name"
-  virtual_network_name = "azurerm_virtual_network.demovnet.name"
-  address_prefixes     = ["10.0.101.0/24"]
-}
-
-resource "azurerm_subnet" "private2" {
-  name                 = "private2-subnet"
-  resource_group_name  = "azurerm_resource_group.demo.name"
-  virtual_network_name = "azurerm_virtual_network.demovnet.name"
-  address_prefixes     = ["10.0.102.0/24"]
-}
-
-resource "azurerm_subnet" "private3" {
-  name                 = "private3-subnet"
-  resource_group_name  = "azurerm_resource_group.demo.name"
-  virtual_network_name = "azurerm_virtual_network.demovnet.name"
-  address_prefixes     = ["10.0.103.0/24"]
+resource "azurerm_public_ip" "wordpress" {
+  name                = "wordpress-public-ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.wordpress.name
+  allocation_method   = "Static"
+  domain_name_label   = random_string.fqdn.result
+  tags                = var.tags
 }
